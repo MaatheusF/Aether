7f3076a7c3f349d1e9699a6b23350fcab4d91838
MaatheusF <91267900+MaatheusF@users.noreply.github.com>
2026-07-18 17:18:36 -0300

    Feature(WEB): Atualiza o status do core no front utilizando API do Core

---
+++ aether-web/public/js/services/api_status.js
@@ -0,0 +1,41 @@
+/* Função que consulta o status do Core */
+async function getCoreStatus() {
+    try {
+        const response = await fetch('http://192.168.0.133:9001/api/status');
+        if (!response.ok) {
+            throw new Error('Core unavailable');
+        }
+        const data = await response.json();
+        setCoreStatus(data);
+        return data;
+    } catch (error) {
+        console.error('Erro ao consultar o Core:', error);
+        setCoreStatus(error);
+        return { status: 'error', message: error.message };
+    }
+}
+
+/* Função que atualiza a exibição status do Core no Front */
+function setCoreStatus(status) {
+    const dot_status = document.getElementById("core-status-dot");
+    const label_status = document.getElementById("core-status-label");
+
+    if(status.status === "UP") {
+        dot_status.classList.remove("bg-aether-critico");
+        dot_status.classList.add("bg-aether-broto");
+
+        label_status.classList.remove("text-aether-critico");
+        label_status.classList.add("text-aether-broto");
+        label_status.textContent = "Core Online"
+    } else {
+        dot_status.classList.remove("bg-aether-broto");
+        dot_status.classList.add("bg-aether-critico");
+
+        label_status.classList.remove("text-aether-broto");
+        label_status.classList.add("text-aether-critico");
+        label_status.textContent = "Core Offline"
+    }
+}
+
+document.addEventListener("DOMContentLoaded", getCoreStatus);
+setInterval(getCoreStatus, 60000); /* Atualiza o status a cada 60 segundos */
